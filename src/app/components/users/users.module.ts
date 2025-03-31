5cba362e41cb3bfcca74dcebc39328baffbf6107
eliudarango <eliudmontiel@outlook.com>
2025-03-31 12:03:57 -0600

    v3

--- src/app/components/users/users.module.ts
+++ src/app/components/users/users.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { UserService } from './user.service'; 
+import { UserService } from './user.service';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserEditComponent } from './user-edit/user-edit.component'; 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserCreateComponent,
+    UserEditComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
