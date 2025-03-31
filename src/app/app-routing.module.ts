652c4178a1902bcfaa9b7ae4b51fa088a53b975a
eliudarango <eliudmontiel@outlook.com>
2025-03-31 13:45:49 -0600

    v4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserTableComponent } from './components/users/user-table/user-table.component'; 
+import { UserCreateComponent } from './components/users/user-create/user-create.component'; 
+import { UserEditComponent } from './components/users/user-edit/user-edit.component'; 
+
 
 
 const routes: Routes = [
   { path: '', component: UserTableComponent },
-
+  { path: 'users/create', component: UserCreateComponent },
+  { path: 'users/edit/:id', component: UserEditComponent }
 ];
 
 @NgModule({
--- src/app/components/users/user-table/user-table.component.ts
+++ src/app/components/users/user-table/user-table.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../../../models/user';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  styleUrls: ['./user-table.component.css']
+  styleUrls: ['./user-table.component.css'],
+  standalone: true,
+  imports: [MatTableModule, MatButtonModule, MatIconModule] 
 })
 export class UserTableComponent implements OnInit {
   users: User[] = [];
+  displayedColumns: string[] = ['id', 'name', 'email', 'actions']; 
 
   constructor(private userService: UserService) { }
 
